a2a5d896e8583f1c81eb6d4ee9f5e9161902be0d
Barigamb738 <fillesdaniel738@gmail.com>
2020-09-22 13:00:06 +0200

    a

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Math;
+using Random = Math.Random;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How much numbers should be generated?");
+            double a = double.Parse(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("What is the minimum?");
+            int min = int.Parse(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("What is the maximum?");
+            int max = int.Parse(Console.ReadLine());
+            Console.Clear();
+            for (double i = 0; i <= a; i++)
+                Console.WriteLine(Random.Int(min, max));
+            Console.ReadKey();
+        }
+    }
+}
