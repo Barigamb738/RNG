6d3987c29e22e1cf53caf802e2a39d6ac7494a3a
Barigamb738 <fillesdaniel738@gmail.com>
2020-09-22 12:38:49 +0200

    added code

---
+++ 2hourRNG/2hourRNG/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Math;
+using Random = Math.Random;
+
+namespace _2hourRNG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(double i = 0; i <= 100; i++)
+                Console.WriteLine(Random.Float(-10, 10));
+        }
+    }
+}
